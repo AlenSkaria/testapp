0e62b6a2b2427d2aca9b714b6273fbf44bcdaf57
AlenSkaria <hello.skaria@gmail.com>
2025-04-20 23:42:27 +0530

    ho

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline hi">Hello world!</h1>;
+  return (
+    <div className="flex h-screen justify-center items-center">
+      <h1 className="text-sm">Hello world😋</h1>;
+    </div>
+  );
 }
